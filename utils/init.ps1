4d98255abdbd86aefd1b57c84be25f4c7392a185
Tommaso Bucaioni <79274762+Tommaso23@users.noreply.github.com>
2025-05-31 17:37:59 +0200

    Delete utils directory

--- utils/init.ps1
+++
@@ -1,5 +0,0 @@
-helm install gitlab gitlab/gitlab 
---set postgresql.install=false 
---set global.psql.host=postgre-aks-test-itn.postgres.database.azure.com
---set global.psql.password.secret=azure-postgresql-password
---set global.psql.password.key=postgres-password --set global.psql.database=postgres
